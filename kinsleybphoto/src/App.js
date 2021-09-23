9a2ce625be9afd6f920fa8da7a3236bba4a5a743
Mari-8 <ryanaltimari8@gmail.com>
2021-09-22 20:40:50 -0500

    Added Router with homepage route, nav, and footer

--- kinsleybphoto/src/App.js
+++ kinsleybphoto/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import HomePage from './containers/HomePage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar /> 
+          <Route exact path='/' component={HomePage} /> 
+          {/* <Route exact path='/galleries' component={GalleriesPage} />
+          <Route exact path='/booking' component={BookingPage} /> 
+          <Route exact path='/about' component={AboutMePage} /> 
+         */}
+        <Footer /> 
+      </Router>
   );
 }
 
