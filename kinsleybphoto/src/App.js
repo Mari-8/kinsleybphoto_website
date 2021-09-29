8e15e3083f2a0468c061fd03c3a58009c65a1172
Mari-8 <ryanaltimari8@gmail.com>
2021-09-29 10:15:28 -0500

    Changed app import

--- kinsleybphoto/src/App.js
+++ kinsleybphoto/src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 import HomePage from './containers/HomePage'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import NavTest from './components/NavTest'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <Navbar /> 
+        {/* <Navbar />  */}
+          <NavTest /> 
           <Route exact path='/' component={HomePage} /> 
           {/* <Route exact path='/galleries' component={GalleriesPage} />
           <Route exact path='/booking' component={BookingPage} /> 
           <Route exact path='/about' component={AboutMePage} /> 
          */}
-        <Footer /> 
-      </Router>
+          <Footer /> 
+    </Router>
   );
 }
 
