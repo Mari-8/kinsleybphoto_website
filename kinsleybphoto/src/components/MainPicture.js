171989522f6c3d99b7a434923c6740d0180424c1
Mari-8 <ryanaltimari8@gmail.com>
2021-09-29 10:13:47 -0500

    Updated main picture

---
+++ kinsleybphoto/src/components/MainPicture.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+//import Image from 'react-bootstrap/Image'
+
+export default function MainPicture() {
+    return (
+        <div className="jumbo-1 text-center ">
+            <Container className="position-absolute top-50 start-50 translate-middle text-white">
+                <h1 className="main-text ">Kinsley B Photography</h1>
+            </Container>
+        </div>
+    )
+}
