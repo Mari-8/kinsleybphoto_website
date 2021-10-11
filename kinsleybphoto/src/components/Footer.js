a99f8d28026cd1657573d6fba0b73141fc656cfc
Mari-8 <ryanaltimari8@gmail.com>
2021-10-10 21:46:05 -0500

    Added first footer column

--- kinsleybphoto/src/components/Footer.js
+++ kinsleybphoto/src/components/Footer.js
@@ -7,17 +7,18 @@ import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className="container-fluid footer">
-      <div className="row">
-        <div className="col">
-
+      <div className="row footer-column">
+        <div className="col footer-column">
+          <h1 className="kins">Kinsley B Photography</h1>
+          <p>Connect with me on my social media! I post regularly and it is another great way to see my work</p>
         </div>
 
-        <div className="col"> 
-
+        <div className="col footer-column"> 
+          <h1>Hello</h1>
         </div>
 
-        <div className="col">
-
+        <div className="col footer-column">
+          
         </div>
 
       </div>
